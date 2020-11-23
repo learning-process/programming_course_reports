a0f33d950830fa2ec322479612ed26cd770856bb
Nesterov Alexander <nesoldr@mail.ru>
2020-11-23 05:25:39 +0300

    update build arc

--- scripts/convert_tex_to_pdf.py
+++ scripts/convert_tex_to_pdf.py
@@ -1,6 +1,6 @@
-from pdflatex import PDFLaTeX
+import pdflatex
 import sys
 
 filename = sys.argv[1]
-pdfl = PDFLaTeX.from_texfile(filename)
-pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
+pdf_descriptor = pdflatex.PDFLaTeX.from_texfile(filename)
+pdf, log, completed_process = pdf_descriptor.create_pdf(keep_pdf_file=True, keep_log_file=True)
